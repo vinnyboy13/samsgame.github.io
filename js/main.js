d84b9d2c3730de3b369117a1f76997eee62807b5
Vincent Hoffman <vince_hoffman@hotmail.com>
2025-08-25 20:29:02 -0500

    Added names and changed chaser.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function () {
   const canvas = document.getElementById("myCanvas");
   const ctx = canvas.getContext("2d");
-  const chaserImage = document.getElementById("VikingsHelmet");
+  const chaserImage = document.getElementById("Cheer");
 
   const runButton = document.getElementById("runButton");
 
@@ -67,6 +67,7 @@
     ctx.arc(chaserX, chaserY, chaserRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
+    ctx.closePath();
     // Viking Helmet
     ctx.drawImage(
       chaserImage,
@@ -75,7 +76,6 @@
       100,
       100
     );
-    ctx.closePath();
   }
 
   function drawPlayerInit() {
